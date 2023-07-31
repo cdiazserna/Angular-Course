88f4eb80e57f53e0830951be9d2987b4aaf0b708
Carlor Jr Diaz <cjunior90@gmail.com>
2023-07-31 18:18:37 -0500

    Example to desestructing objects

--- src/topics/05-destructuring.ts
+++ src/topics/05-destructuring.ts
@@ -1,3 +1,37 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
 
+interface Details {
+    author: string;
+    year: number;
+}
+
+const audioPlayer : AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: 'Ed Sheeran',
+        year: 2015
+    }
+}
+
+const song = 'New Song';
+
+const { 
+    song: anotherSong,
+    songDuration: duration,
+    details: details } = audioPlayer;
+
+const { author } = details;
+
+console.log('Song1: ', anotherSong);
+console.log('Duration: ', duration);
+console.log('Song2: ', song);
+console.log('Author: ', author);
 
 export {};
